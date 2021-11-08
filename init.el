9d7c9f133197fef488355e705effad5245c25048
Guillaume Pagnoux <gpagnoux@gmail.com>
2021-11-08 20:01:21 +0100

    vertico: add additional Emacs configs
    Signed-off-by: Guillaume Pagnoux <gpagnoux@gmail.com>

--- init.el
+++ init.el
@@ -215,6 +215,27 @@
   :init
   (marginalia-mode))
 
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; Alternatively try `consult-completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
 ;; Org
 (setf org-todo-keywords '((sequence "PROJ" "TODO" "STRT" "|" "DONE")) ; Extra todo keywords
       org-log-done 'time) ; Log time when marking a todo as done
