d92065dc2243dab4c10a5ddd379629d6fcacb5b4
Guillaume Pagnoux <gpagnoux@gmail.com>
2021-11-09 17:39:19 +0100

    ui: enable all-the-icons-dired
    Signed-off-by: Guillaume Pagnoux <gpagnoux@gmail.com>

--- init.el
+++ init.el
@@ -165,6 +165,9 @@
 ;; Fill column indicator
 (add-hook! prog-mode-hook display-fill-column-indicator-mode)
 
+;; Fancy icons in dired
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
 
 ;;; Packages
 
