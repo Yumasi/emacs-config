abd7b91405474c7ca0733998a9b5b523ef0c4fb2
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-04-23 14:46:16 +0200

    magit: add keybinding for magit-status

--- modules/ym-magit.el
+++ modules/ym-magit.el
@@ -1,5 +1,8 @@
 (use-package magit
   :config
-  (setf transient-values '((magit-commit "--allow-empty"))))
+  (setf transient-values '((magit-commit "--allow-empty")))
+  :general
+  (ym/leader-def 'normal
+    "g g" 'magit-status))
 
 (provide 'ym-magit)
