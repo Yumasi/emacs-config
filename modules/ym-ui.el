b95282c2da16f9f538a68815988064d8d8335274
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-05-07 20:55:51 +0200

    ui: remove nano-modeline

--- modules/ym-ui.el
+++ modules/ym-ui.el
@@ -1,30 +1,6 @@
-(defun ym-disable-modeline ()
-  (interactive)
-  (setq mode-line-format nil))
-
-(defun ym-after-init-scratch ()
-  (with-current-buffer "*scratch*"
-    (ym-disable-modeline)
-    (nano-modeline-text-mode)))
-
-(use-package nano-modeline
-  :hook (((prog-mode text-mode magit-mode) . ym-disable-modeline)
-         (magit-mode . nano-modeline-text-mode)
-         (org-mode . nano-modeline-org-mode)
-         (prog-mode . nano-modeline-prog-mode)
-         (text-mode . nano-modeline-text-mode)
-         (after-init . ym-after-init-scratch))
-  :config
-  (nano-modeline-prog-mode t))
-
 (use-package nano-theme
   :ensure (:host github :repo "rougier/nano-theme")
   :config
   (load-theme 'nano t))
-
-;;(use-package nano-minibuffer
-;;  :ensure (:host github :repo "rougier/nano-minibuffer")
-;;  :config
-;;  (nano-minibuffer-mode))
 
 (provide 'ym-ui)
