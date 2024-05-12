817f1c94734ab112bb0d4c3e563f2ef3fde5c3d6
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-05-12 19:04:18 +0200

    ui: setup modus theme

--- modules/ym-ui.el
+++ modules/ym-ui.el
@@ -1,6 +1,18 @@
-(use-package nano-theme
-  :ensure (:host github :repo "rougier/nano-theme")
-  :config
-  (load-theme 'nano t))
+;; Setup then load Modus themes
+(setq modus-themes-bold-constructs t
+      modus-themes-italic-constructs t
+      modus-themes-syntax '(alt-syntax)
+      modus-themes-mixed-fonts t
+      modus-themes-links '(italic background)
+      modus-themes-mode-line '(accented)
+      modus-themes-completions '((matches . (extrabold))
+                                 (selection . (accented intense)))
+      modus-themes-fringes '(intense)
+      modus-themes-hl-line '(accented)
+      modus-themes-subtle-line-numbers t
+      modus-themes-paren-match '(intense bold)
+      modus-themes-region '(accented)
+      modus-themes-variable-pitch-ui t)
+(load-theme 'modus-vivendi)
 
 (provide 'ym-ui)
