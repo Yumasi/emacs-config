b66314b2551d7db7254a793f6622b09271af0de3
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-05-14 14:06:34 +0200

    treesitter: use tree-sitter-langs for grammars installation

--- modules/ym-lsp.el
+++ modules/ym-lsp.el
@@ -1,7 +1,8 @@
 (use-package go-mode)
 
-(general-add-hook '(c-mode-hook
-                    go-mode-hook)
+(general-add-hook '(c-ts-mode-hook
+                    go-ts-mode-hook
+                    python-ts-mode-hook)
                   'eglot-ensure)
 
 (provide 'ym-lsp)
--- modules/ym-treesitter.el
+++ modules/ym-treesitter.el
@@ -1,16 +1,12 @@
-(defmacro treesit-install-unless-available (lang)
-  `(unless (treesit-language-available-p ',lang)
-     (treesit-install-language-grammar ',lang)))
+(use-package tree-sitter-langs
+  :config
+  (tree-sitter-langs-install-latest-grammar t))
 
-;; Populate the sources for treesitter's language libraries.
-(setq treesit-language-source-alist
-      '((elisp "https://github.com/Wilfred/tree-sitter-elisp")
-        (go "https://github.com/tree-sitter/tree-sitter-go")))
+(setq major-mode-remap-alist '((c-mode . c-ts-mode)
+                               (c++-mode . c++-ts-mode)
+                               (go-mode . go-ts-mode)
+                               (python-mode . python-ts-mode)))
 
-(treesit-install-unless-available elisp) ; TODO: figure out the correct major mode for this
-(treesit-install-unless-available go)
-
-(setq major-mode-remap-alist
-      '((go-mode . go-ts-mode)))
+(customize-set-variable 'treesit-font-lock-level 4)
 
 (provide 'ym-treesitter)
