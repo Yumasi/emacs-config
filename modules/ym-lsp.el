a8df0dd74be826ae69754f750e12d6beb80cf3ca
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-05-15 12:06:43 +0200

    lsp: format eglot-managed buffers on save

--- modules/ym-lsp.el
+++ modules/ym-lsp.el
@@ -1,8 +1,18 @@
 (use-package go-mode)
 
+(defun ym-eglot-format-on-save ()
+  "This function is meant to be used in BEFORE-SAVE-HOOK.
+Formats the buffer if eglot is managing it."
+
+  (when (eglot-managed-p)
+    (eglot-format-buffer)))
+
+
 (general-add-hook '(c-ts-mode-hook
                     go-ts-mode-hook
                     python-ts-mode-hook)
                   'eglot-ensure)
+
+(add-hook 'before-save-hook #'ym-eglot-format-on-save)
 
 (provide 'ym-lsp)
