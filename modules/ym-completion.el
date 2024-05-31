536288b0dd747a63d8b01f04414eca687741199a
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-05-31 11:23:32 +0200

    completion: add popupinfo

--- modules/ym-completion.el
+++ modules/ym-completion.el
@@ -6,9 +6,12 @@
 (use-package corfu
   :init
   (global-corfu-mode)
+  (corfu-popupinfo-mode 1)
   :bind
   (:map corfu-map ("C-SPC" . corfu-insert-separator))
   :custom
-  (corfu-auto t))
+  (corfu-auto t)
+  (corfu-popupinfo-delay '(1.0 . 1.0))
+  (corfu-popupinfo-max-height 20))
 
 (provide 'ym-completion)
