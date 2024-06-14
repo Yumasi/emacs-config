f0e5ad09cd0c4ba454fefa7903d87c2a1be46f97
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-06-14 15:59:00 +0200

    project: replace VC with Magit

--- modules/ym-project.el
+++ modules/ym-project.el
@@ -4,6 +4,13 @@
 (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
 (setq compilation-scroll-output t)
 
+(setq project-switch-commands
+      '((project-find-file "Find file")
+        (project-find-regexp "Find regexp")
+        (project-find-dir "Find directory")
+        (magit-project-status "Magit" ?v)
+        (project-eshell "Eshell")))
+
 (ym/leader-def
   :states 'normal
   :keymaps 'override
