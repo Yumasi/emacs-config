e543297909895ef09d73801dd8787a9598e16d8e
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-06-15 17:23:29 +0200

    corfu: add corfu-terminal and kind-icon

--- modules/ym-completion.el
+++ modules/ym-completion.el
@@ -14,4 +14,15 @@
   (corfu-popupinfo-delay '(1.0 . 1.0))
   (corfu-popupinfo-max-height 20))
 
+(use-package corfu-terminal
+  :after corfu
+  :init
+  (unless (display-graphic-p)
+    (corfu-terminal-mode +1)))
+
+(use-package kind-icon
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
 (provide 'ym-completion)
