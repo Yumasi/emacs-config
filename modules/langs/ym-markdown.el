247c04522ecb8aaf30e8a0b2dfbabc4e5bcc4a7f
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-07-16 15:34:05 +0200

    langs: add markdown

---
+++ modules/langs/ym-markdown.el
@@ -0,0 +1,4 @@
+(use-package markdown-mode
+  :mode ("README\\.md\\'" . gfm-mode))
+
+(provide 'ym-markdown)
--- modules/ym-langs.el
+++ modules/ym-langs.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path (concat (file-name-as-directory user-emacs-directory) "modules/langs"))
 
 (require 'ym-guile)
+(require 'ym-markdown)
 (require 'ym-zig)
 
 (provide 'ym-langs)
